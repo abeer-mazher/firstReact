fc70ee50f2aba034e8aa8b68b0dfc00533012b03
abeer-mazher <159471490+abeer-mazher@users.noreply.github.com>
2024-06-29 00:55:56 +0500

    Add files via upload

---
+++ firstReact/src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+        <h1>Hello React</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit, autem.
+        </p>
+        <input type="text" />
+        <br />
+        <img src="/vite.svg" alt="" />
+      </div>
+    </>
+  )
+}
+
+export default App
